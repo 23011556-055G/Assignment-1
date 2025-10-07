432daa4d1563af73cb7460d6751125ce70e5d5f7
23011556-055G <noornasar419@gmail.com>
2025-10-07 17:36:52 +0500

    Add files via upload

---
+++ Qno6(factorial calculation by using while loop).cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace App
+{
+    class FactorialCalculator
+    {
+        static void Main()
+        {
+            Console.WriteLine("Factorial Calculator");
+            Console.WriteLine("---------------------");
+
+            Console.Write("Enter a non-negative number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("Factorial is not defined for negative numbers.");
+            }
+            else
+            {
+                long factorial = 1;
+                int i = 1;
+
+                while (i <= number)
+                {
+                    factorial *= i;
+                    i++;
+                }
+
+                Console.WriteLine("Factorial of " + number + " is: " + factorial);
+            }
+        }
+    }
+}
+
