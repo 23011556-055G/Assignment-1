94dacb76f894fcb41e681358575b4539cfa4ed2f
23011556-055G <noornasar419@gmail.com>
2025-10-07 17:50:56 +0500

    Add files via upload

---
+++ Qno7(Reversed number).cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace App
+{
+    class ReverseDigits
+    {
+        static void Main()
+        {
+            Console.WriteLine("Reverse Digits of a Number");
+            Console.WriteLine("--------------------------");
+
+            Console.Write("Enter an integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int originalNumber = number;
+            int reversed = 0;
+
+            while (number != 0)
+            {
+                int digit = number % 10;     
+                reversed = reversed * 10 + digit; 
+                number = number / 10;            
+            }
+
+            Console.WriteLine("Reversed number of " + originalNumber + " is: " + reversed);
+        }
+    }
+}
+
