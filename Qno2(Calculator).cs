4ba02f47be04683e15311e3542616edf74f75b15
23011556-055G <noornasar419@gmail.com>
2025-10-07 17:04:48 +0500

    Add files via upload

---
+++ Qno2(Calculator).cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace App
+{
+    class ProgramCalculator
+    {
+        static void Main()
+        {
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("-----------------");
+
+           
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+           
+            Console.Write("Enter an operator (+, -, *, /): ");
+            string op = Console.ReadLine();
+
+            
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot divide by zero.");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    break;
+            }
+        }
+    }
+}
