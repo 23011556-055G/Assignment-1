2347dbdf0eb1d31d6031d8f77447a839227d575c
23011556-055G <noornasar419@gmail.com>
2025-10-07 17:30:23 +0500

    Add files via upload

---
+++ Qno5(Multiplication Table).cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace App
+{
+    class MultiplicationTable
+    {
+        static void Main()
+        {
+            Console.WriteLine("Multiplication Table");
+            Console.WriteLine("---------------------");
+
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nMultiplication Table of " + number + ":");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + " x " + i + " = " + (number * i));
+            }
+        }
+    }
+}
+
+
