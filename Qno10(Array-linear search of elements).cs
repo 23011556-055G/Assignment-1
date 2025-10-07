2969a956e8b6e104ba89f75d8031bfe5fef1ed3a
23011556-055G <noornasar419@gmail.com>
2025-10-07 18:27:04 +0500

    Add files via upload

---
+++ Qno10(Array-linear search of elements).cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace App
+{
+    class LinearSearch
+    {
+        static void Main()
+        {
+            int[] numbers = new int[10];
+
+            Console.WriteLine("Enter 10 integers:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Number " + (i + 1) + ": ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("\nEnter a number to search: ");
+            int searchValue = Convert.ToInt32(Console.ReadLine());
+
+            
+            bool found = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == searchValue)
+                {
+                    Console.WriteLine("Number found at position " + (i + 1));
+                    found = true;
+                    break;
+                }
+            }
+
+            
+            if (!found)
+            {
+                Console.WriteLine("Number not found in the array.");
+            }
+        }
+    }
+}
+
