6072379446147655a45901f8bb204d9fe795feef
23011556-055G <noornasar419@gmail.com>
2025-10-07 18:01:20 +0500

    Add files via upload

---
+++ Qno9(Array-count even and odd number).cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace App
+{
+    class EvenOddCounter
+    {
+        static void Main()
+        {
+            int[] numbers = new int[10];
+            int evenCount = 0;
+            int oddCount = 0;
+
+            Console.WriteLine("Enter 10 integers:");
+
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("Number " + (i + 1) + ": ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (numbers[i] % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("\nTotal Even Numbers: " + evenCount);
+            Console.WriteLine("Total Odd Numbers: " + oddCount);
+        }
+    }
+}
