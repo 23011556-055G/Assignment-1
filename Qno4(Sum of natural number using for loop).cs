56b4ea617064539b47349d8637463d8dea9afff7
23011556-055G <noornasar419@gmail.com>
2025-10-07 17:24:59 +0500

    Add files via upload

---
+++ Qno4(Sum of natural number using for loop).cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace App
+{
+    class SumNaturalNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Sum of Natural Numbers");
+            Console.WriteLine("----------------------");
+
+            Console.Write("Enter a positive number (n): ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n < 1)
+            {
+                Console.WriteLine("Please enter a number greater than 0.");
+            }
+            else
+            {
+                int sum = 0;
+
+                for (int i = 1; i <= n; i++)
+                {
+                    sum += i;
+                }
+
+                Console.WriteLine("The sum of natural numbers from 1 to " + n + " is: " + sum);
+            }
+        }
+    }
+}
+
